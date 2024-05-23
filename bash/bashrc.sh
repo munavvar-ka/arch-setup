f9617c82b7fca0d87f3b11bab448ec1dd7e55693
Munavvar K A <munavvar.ka@gmail.com>
2024-05-23 20:31:10 +0530

    DISPLAY COMMAND RUNNING TIME

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -9,9 +9,13 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
 source ~/.git-prompt.sh
-PS1='\n\[\e[38;5;208;1;2;3m\]#\# \[\e[0m\][\[\e[92;1;3m\]\u\[\e[0;38;5;196m\]@\[\e[38;5;33;1;3m\]\h\[\e[0m\] \[\e[38;5;118;2m\]\w\[\e[0m\]] \[\e[96;2m\][\D{%I:%M:%S %p}] \[\e[38;5;51;1;3m\]$(__git_ps1 "  %s") \n\[\e[0;1;5m\]>> \[\e[0m\]'
+source ~/.bash/run-time.sh
+
+PS1='\n\[\e[38;5;208;1;2;3m\]#\# \[\e[11;38;5;104m\][#$((\#-1)) ${timer_show}s]\[\e[0m\]';
+PS1="${PS1} \[\e[0m\][\[\e[92;1;3m\]\u\[\e[0;38;5;196m\]@\[\e[38;5;33;1;3m\]\h\[\e[0m\] \[\e[38;5;118;2m\]\w\[\e[0m\]]"
+PS1="${PS1} \[\e[96;2m\][\D{%I:%M:%S %p}]"
+PS1="${PS1} \n\[\e[0;1;5m\]>> \[\e[0m\]"
 
 # ============== Alias =========== #
-
 source ~/.bash/artisan.sh
 source ~/.bash/git.sh
